e97804e32d5d49f4fab18661805d95de64eeb09f
Franco Romagnoli <franco.romagnoli@techforb.com>
2025-05-12 09:56:36 -0300

    CHG: ALL: Se eliminan archivos en desuso

--- backend/src/main/java/com/mercadolibre/app_geo/service/interfaces/CountryService.java
+++
@@ -1,9 +0,0 @@
-package com.mercadolibre.app_geo.service.interfaces;
-
-import com.mercadolibre.app_geo.model.CountryEntity;
-
-public interface CountryService {
-
-    CountryEntity findOrCreate(String countryName, String countryCode);
-
-}
